16734a38218362bde9cbd2d26668f4627874616f
Daianes26 <daianeasilva26@gmail.com>
2021-05-12 19:17:49 -0400

    handleChange Method

--- src/App.js
+++ src/App.js
@@ -13,12 +13,18 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
+    
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value})
   }
 
 
@@ -34,8 +40,7 @@ class App extends Component {
       <div className="App">
         <SearchBox 
           placeholder= 'search monsters'
-          handleChange = {e => 
-          this.setState({ searchField: e.target.value})}
+          handleChange = {this.handleChange}
         />
         <CardList monsters = {filteredMonsters}></CardList>
        
